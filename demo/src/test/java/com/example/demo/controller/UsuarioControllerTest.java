8842a9751308aff6a1ebb55c7c72610b336ff160
Giovanny Otarola <giovannyxdd@gmail.com>
2024-04-23 18:46:17 -0400

    Update UsuarioControllerTest.java

--- demo/src/test/java/com/example/demo/controller/UsuarioControllerTest.java
+++ demo/src/test/java/com/example/demo/controller/UsuarioControllerTest.java
@@ -1,6 +1,5 @@
 package com.example.demo.controller;
 
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 
@@ -10,7 +9,6 @@ import java.util.Optional;
 
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentMatchers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
